eb39e7694b59476392b94476cf3ef0d1dc1eef36
Alejandro Montoya <almontoya02@outlook.com>
2022-03-25 13:06:58 -0500

    monitoring

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGODB_URL)
 const userRouter = require("./src/routes/user")
 const recordRouter = require("./src/routes/record")
+const Airbrake = require('@airbrake/node');
+
+new Airbrake.Notifier({
+    projectId: 406523,
+    projectKey: 'e6d1172b03271cd9e66e4360dae52a28',
+    environment: 'production'
+  });
+
 
 app.use(express.json())
 app.use(cors())
