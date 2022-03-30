8c1985c1784753213c1d6a168835c09713d99ef4
Alejandro Montoya <almontoya02@outlook.com>
2022-03-30 10:54:47 -0500

    Test error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require("cors")
 const app = express()
-const port = process.env.PORT || 4001
+const port = ""
 require("dotenv").config()
 const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGODB_URL)
