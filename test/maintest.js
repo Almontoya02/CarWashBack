4dc1ebda9f3b0194ac00bf6184ccabb53f1b479c
Alejandro Montoya <almontoya02@outlook.com>
2022-03-31 18:58:16 -0500

    Tests11

--- test/maintest.js
+++ test/maintest.js
@@ -1,83 +1,33 @@
-const expect = require("chai").expect;
+const expect = require("chai");
 const request = require("supertest");
 const server =  require("../index");
 var app = request.agent(server);
+var assert = require('assert');
 
 const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsbW9uQGdnLmNvbSIsIm5hbWUiOiJBbGVqaXRvIE1vbnRveWl0YSIsImlhdCI6MTY0ODc1NzIzN30.d82FVTRvEgCcigTLmdrLJ0pim9rzmYmvsH5zu3Pc4fQ"
 
-describe("post request",function(){
-    describe("Agregar un nuevo registro de lavado sin token valido",function(){
-        it("Status deberia retornar false",function(){
-            app.post("/record/create").send({
-                workerId:"10255555",
-                workerName:"albernual gomez",
-                clientId:"164158",
-                clientName:"elmo",
-                clientEmail:"elmo@gg.com",
-                vehicleType:"Automovil",
-                washType:"Brillado",
-                washPrice:50000
-            }).set({
-                "access-token":"nulll",
-                email:"almon@gg.com"
-            }).end((err,res)=>{
-                expect(res.body.status).to.equal(false)
-            })
-        })
-        
-    })
-    describe("Agregar un nuevo registro de lavado con token valido",function(){
-        it("Status deberia retornar true",function(){
-            app.post("/record/create").send({
-                workerId:"105600000",
-                workerName:"Alejito Montoyita",
-                clientId:"164158",
-                clientName:"elmo",
-                clientEmail:"elmo@gg.com",  
-                vehicleType:"Automovil",
-                washType:"Brillado",
-                washPrice:50000
-            }).set({
-                "access-token":token,
-                email:"almon@gg.com"
-            }).end((err,res)=>{
-                expect(res.body.status).to.equal(false)
-            })
-        })
-        
-    })
-})
+describe("Suma",()=>{
+    it("Deberia retornar 12",()=>{
+        let num1 =3;
+        let num2 = 4;
 
-describe("get request",function(){
+        assert.equal(num1*num2,12);
+        
+    });
+});
+
+/**describe("get request",function(){
     describe("Traer registros sin un usuario existente",function(){
-        it("Status deberia retornar true",function(){
-            app.get("/record/getbyuser/10255555222").send().set({
+        it("Status deberia retornar true",async function(){
+            let response = await app.get("/record/getbyuser/10255555222").send().set({
                 "access-token":token,
                 email:"almon@gg.com"
-            }).end((err,res)=>{
-                expect(res.body.status).to.equal(false)
-            })
-        })
-        
-    })
-    describe("Agregar un nuevo registro de lavado con token valido",function(){
-        it("Status deberia retornar true",function(){
-            app.post("/record/create").send({
-                workerId:"105600000",
-                workerName:"Alejito Montoyita",
-                clientId:"164158",
-                clientName:"elmo",
-                clientEmail:"elmo@gg.com",
-                vehicleType:"Automovil",
-                washType:"Brillado",
-                washPrice:50000
-            }).set({
-                "access-token":token,
-                email:"almon@gg.com"
-            }).end((err,res)=>{
-                expect(res.body.status).to.equal(false)
-            })
-        })
-        
-    })
-})
+            }).then((res)=>{
+                console.log(res.body)
+                return res.body.status
+            }).catch(err=>console.log(err));
+            expect(response).to.equal(false)
+        });
+    });
+});
+**/
